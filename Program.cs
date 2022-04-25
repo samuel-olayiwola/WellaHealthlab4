fb36aff1bf8df552495077107f91e0c4058067e1
Samuel Peter <you@example.com>
2022-04-25 10:29:08 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;  
+  public class PrimeNumberExample  
+   {  
+     public static void Main(string[] args)  {
+         Console.Write("Enter first number:");
+         int firstNum = int.Parse(Console.ReadLine());
+         Console.Write("Enter second number:");
+         int secNum = int.Parse(Console.ReadLine());
+         Console.WriteLine(lessThan100(firstNum,secNum));
+
+     }
+
+     static bool lessThan100(double a, double b){
+        if( a + b < 100){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+     }
+   }
